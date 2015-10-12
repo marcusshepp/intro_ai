5fa742099c705c5177c909d66a577211862c7c65
marcusshepp <sheph2mj@cmich.edu>
2015-10-12 18:38:28 -0400

    addng comments to my ga

--- genetic_algorithms/path_to_int/first_ga.cs
+++ genetic_algorithms/path_to_int/first_ga.cs
@@ -6,6 +6,22 @@ namespace GAS
 	class GA
 	{
 
+		/* Path to Int.
+		 * 
+		 * My first take on genetic algorithms.
+		 * Inspiration and idea for problem space
+		 * came from (http://www.ai-junkie.com/ga/intro/gat1.html).
+		 * 
+		 * Summary:
+		 * Finds a path to integer `GOAL` ranging from 0 - 81.
+		 * Using  the following tools:
+		 *     - x = 0 - 9
+		 *     - y = 0 - 9
+		 *     - i = `possible operators`
+		 *
+		 * 
+		 */
+
 		static string [] possible_operators = {"+", "-", "/", "*"};
 		static int GOAL = 10;
 
@@ -19,6 +35,15 @@ namespace GAS
 			}
 			return population;
 		}
+
+		/* Encoding
+		 * 
+		 * The following are methods that perform
+		 * Bit Masking and Bit Shifting.
+		 * extracting the problem from bits 
+		 * creates a way for the computer to
+		 * find a solution faster.
+		*/
 
 		static string get_operator(int chrome){
 			/* 0000 0000 11 */
@@ -47,6 +72,10 @@ namespace GAS
 			return (chrome & 31);
 		}
 
+		/*
+		 * Decoding
+		*/
+
 		static int decode(int chrome){
 			/* decodes the chromosome and returns its functions result. */
 			int first_number = get_first_number (chrome);
@@ -74,6 +103,9 @@ namespace GAS
 		}
 
 		static bool is_solution(int chromosome){
+			/* :returns: true if is solution 
+			 *           else false
+			*/
 			int goal = GOAL;
 			int value = decode (chromosome);
 			if (value == goal) {
@@ -84,18 +116,13 @@ namespace GAS
 		}
 
 		static int fitness_score(int chrome) {
-			/* evaluates the chromosomes and outputs their fitness score. */
+			/* evaluates a chromosome and outputs its fitness score. 
+			 * highly dependant on GOAL
+			*/
 			int goal = GOAL;
 			int score = Math.Abs (goal - decode(chrome));
 			return score;
 		}
-
-			/* select two chromosomes based on fitness score. 
-			 * decode the chromosomes. the two parent chromos
-			 * have a .5 chance of being crossed over at there
-			 * center most digit. if they dont crossover their
-			 * children will be exact copies.
-			*/
 
 		static int select_partner(int[] population){
 			/* 
@@ -104,7 +131,7 @@ namespace GAS
 			 * lower fitness (closer to 0)
 			 * gets to breed.
 			 * 
-			*/ 
+			 */ 
 			Random rand = new Random ();
 			int a = rand.Next (0, 100);
 			int b = rand.Next (0, 100);
@@ -119,14 +146,19 @@ namespace GAS
 
 		static int[] crossover(int[] parents){
 			/*
-			 * in: parents;
-			 * out: childone, childtwo;
-			*/
+			 * in: parents[2];
+			 * out: children[2];
+			 */
 			Random rand = new Random ();
 			int choice = rand.Next (0, 100);
 			int[] children = new int[2];
 			if (choice > 50) {
-				// crossover
+				/* crossover
+				 * p0 = 0000011111
+				 * p1 = 1111100000
+				 * c0 = 1111111111
+				 * c1 = 0000000000
+				 */ 
 				int male_first_half = get_first_half (parents [0]);
 				int male_second_half = get_second_half (parents [0]);
 				int female_first_half = get_first_half (parents [1]);
@@ -146,9 +178,13 @@ namespace GAS
 			/* 
 			 * mutate the given decimal
 			 * by randomly choosing a bit
-			 * and XOR it with its opposite.
+			 * and XOR it with a one.
 			 * 0 xor 1 = 1
 			 * 1 xor 1 = 0
+			 * 
+			 * *note*
+			 * must shift bit to desired allel
+			 * 
 			 */
 			Random rand = new Random ();
 			int bit_to_flip = rand.Next (0, 10);
@@ -158,7 +194,23 @@ namespace GAS
 
 
 		static void Main(string[] args)
-		{	
+		{	/* def PATHTOINT ( GOAL ) do
+		     *     initialize pop whichis population
+		     *     int generations
+		     *     for (gen whichis 0 to generations) do
+		     *         new_pop = []
+		     *         if is_empty(pop) do
+		     *             pop = new_pop
+		     *         for (pop_index whichis 0 to pop.length) do
+		     *             if is_solution(pop[pop_index]) do
+		     *                 return pop[pop_index]
+		     *             parents = [pop[pop_index], select_partner(pop)]
+		     * 			   children = [mutate(crossover(parents))]
+		     * 		       child = best(children)
+		     * 			   new_pop.append(child)
+		     *		   	   pop.remove(pop_index)
+		     *     return best_answer(pop)
+		     */
 			int population_size = 100;
 			int[] population = initialize_population (population_size);
 			int num_of_generations = 100;
@@ -166,12 +218,19 @@ namespace GAS
 			for (int generations = 0; generations <= num_of_generations; generations++) {
 				List<int> new_pop = new List<int>();
 				for (int x = 0; x < 100; x++) {
+
+					// select parents
 					int[] parents = new int[2];
 					parents[0] = population [x];
 					parents[1] = select_partner (population);
+
+					// crossover parents into children
 					int[] children = crossover(parents);
+
+					// mutate children
 					int child_one = mutate (children [0]);
 					int child_two = mutate (children [1]);
+
 					// only add the child with the best fitness score
 					// this way the population size remains the same
 					if (fitness_score (child_one) < fitness_score (child_two)) {
