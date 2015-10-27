1101efa92d0fd0b0086252eb33a65baf9b8ec27b
marcusshepp <sheph2mj@cmich.edu>
2015-10-27 15:44:21 -0400

    DONE BITCH

--- genetic_algorithms/path_to_int/path_to_int_two_point_oh.py
+++ genetic_algorithms/path_to_int/path_to_int_two_point_oh.py
@@ -21,10 +21,10 @@ def decode(chromosome):
     out: decoded result
     """
     first_number = (chromosome & 255)
-    second_number = (chromosome & 65280 >> 8)
-    third_number = (chromosome & 16711680 >> 16)
-    fourth_number = (chromosome & 4278190080 >> 24)
-    print first_number, " + ", second_number, " + ", third_number, " + ", fourth_number
+    second_number = ((chromosome & 65280) >> 8)
+    third_number = ((chromosome & 16711680) >> 16)
+    fourth_number = ((chromosome & 4278190080) >> 24)
+    # print first_number, " + ", second_number, " + ", third_number, " + ", fourth_number
     chrome_sum = first_number + second_number + third_number + fourth_number
     return chrome_sum
 
@@ -41,9 +41,9 @@ def select_partner(population):
     for p in population:
         for f in xrange(fitness(p)):
             big_list.append(p)
-    print "big_list: ", big_list
+    # print "big_list: ", big_list
     return big_list[rnd]
-    
+
 
 def crossover(parents):
     """
@@ -54,8 +54,9 @@ def crossover(parents):
     def get_first_half(c):
         return (c & 65535)
     def get_second_half(c):
-        return (c & 4294901760 >> 16)
-    crossover_decision = r.randrange(0, 1)
+        return (c & 4294901760)
+    # crossover_decision = r.randrange(0, 1)
+    crossover_decision = 1
     if crossover_decision:
         male_first_half = get_first_half(parents[0])
         male_second_half = get_second_half(parents[0])
@@ -70,29 +71,53 @@ def crossover(parents):
 def mutate(chromosome):
     """
     in: chromosome
-    out: mutated chromosome 
-    rate: 0.5
+    out: mutated chromosome
+    rate: 1
+    *
+      note if rate is 0.5 takes significantly
+      longer to find solution.
+    *
     """
-    rand_num = r.randrange(0, 1)
-    if rand_num:
-        bit_to_flip = r.randrange(0, 31)
-        mutated_chrome = chromosome ^ (1 << bit_to_flip)
-        return mutated_chrome
-    else: return chromosome
-    
+    bit_to_flip = r.randrange(0, 31)
+    mutated_chrome = chromosome ^ (1 << bit_to_flip)
+    return mutated_chrome
+
 def fitness(chromosome):
     """
     in: chromosome
     out: 1000 - abs(chromosome - GOAL)
     """
     return 1000 - abs(decode(chromosome) - GOAL)
-    
+
+def average_fitness(population):
+    """
+    in: population
+    out: sum of all fitness scores / length of population
+    """
+    fits = [fitness(x) for x in population]
+    return (sum(fits)/len(fits))
+
+def max_fitness(population):
+    fits = [fitness(x) for x in population]
+    return max(fits)
+
+def chrome_to_string(chromosome):
+    first_number = (chromosome & 255)
+    second_number = ((chromosome & 65280) >> 8)
+    third_number = ((chromosome & 16711680) >> 16)
+    fourth_number = ((chromosome & 4278190080) >> 24)
+    chrome = str(first_number) + " + " + str(second_number) + \
+    " + " + str(third_number) + " + " + str(fourth_number)
+    chrome_sum = first_number + second_number + third_number + fourth_number
+    chrome_string = chrome + " = " + str(chrome_sum)
+    return chrome_string
+
 def is_solution(chromosome):
     if decode(chromosome) == GOAL:
         return True
     else: return False
-    
-def main():
+
+def generations(gen_size):
     """
     pop = init_pop
     from 0 to generation do
@@ -107,25 +132,26 @@ def main():
         pop = new_pop
     """
     population = init_pop(50)
-    for i in range(100): # generations
-        print "generation: ", i
+    gen = 0
+    for i in xrange(gen_size): # generations
         temp_population = []
         for chromosome in population:
             if is_solution(chromosome):
                 print "SOLUTION FOUND"
-                print decode(chromosome)
-                return chromosome
-                break
-            parents = [chromosome, select_partner(population)]
+                print chrome_to_string(chromosome)
+                return
+w            parents = [chromosome, select_partner(population)]
             crossed_parents = crossover(parents)
             mutated_child_one = mutate(crossed_parents[0])
             mutated_child_two = mutate(crossed_parents[1])
             if fitness(mutated_child_one) > fitness(mutated_child_one):
                 temp_population.append(mutated_child_one)
-            elif fitness(mutated_child_one) > fitness(mutated_child_one):
+            else:
                 temp_population.append(mutated_child_two)
+        gen += 1
+        print "Generation: ", gen
+        print "Maximum Fitness Score: ", max_fitness(population)
+        print "Average Fitness Score: ", average_fitness(population)
         population = temp_population
 
-main()
-        
-        
+generations(500)
